0bdee95c8ed6d90bfd5cb3db8b0670b75cd6f2eb
juemimgcd <3489161689@qq.com>
2026-03-15 20:14:48 +0800

    nothing

--- conf/db_conf.py
+++ conf/db_conf.py
@@ -19,7 +19,6 @@ async def get_database():
     async with AsyncSessionLocal() as session:
         try:
             yield session
-            await session.commit()
         except Exception as e:
             await session.rollback()
             raise e
